09b954890eab8a882dc1d7081f5ac85e2b90053f
kelompok4 <129141184+famita076@users.noreply.github.com>
2023-12-27 11:00:32 +0700

    Update app.sql

--- app.sql
+++ app.sql
@@ -1,26 +1,90 @@
-drop table if exists schedule;
-create table schedule (
-	id serial,
-	doctor_name text,
-	patient_name text,
-	gender text,
-	symptom text,
-	handphone text,
-	address text,
-	waktu time,
-	tanggal date
-);
+import streamlit as st
+from sqlalchemy import text
 
-insert into schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) 
-values
-	('dr. Nurita', 'Ahmad Maulana', 'male', '["headache", "stomache"]', 62838, 'address1', '08:00', '2023-10-01'),
-	('dr. Yogi', 'Renata Zahab', 'female', '["cough", "flu"]', 62838, 'address2', '09:00', '2022-10-02'),
-	('dr. Wibowo', 'Nunuk Reni', 'female', '["cough", "flu"]', 62838, 'address3', '10:00', '2022-10-03'),
-	('dr. Yogi', 'Bro Ulil', 'male', '["cough", "headache"]', 62838, 'address4', '11:00', '2022-10-04'),
-	('dr. Ulama', 'Wah Bowi', 'male', '["headache", "flu"]', 62838, 'address5', '12:00', '2022-10-05'),
-	('dr. Ulama', 'Iis Mika', 'female', '["cough", "flu", "stomache", "headache"]', 62838, 'address6', '08:00', '2022-10-06'),
-	('dr. Ping', 'Zizah Lana', 'female', '["flu", "stomache", "headache"]', 62838, 'address7', '09:00', '2022-10-07'),
-	('dr. Nurita', 'Alif Iman', 'male', '["cough", "flu", "headache"]', 62838, 'address8', '10:00', '2022-10-08'),
-	('dr. Ping', 'Zaka Zaki', 'female', '["cough", "stomache", "headache"]', 62838, 'address9', '11:00', '2022-10-09'),
-	('dr. Wibowo', 'Faus Rahmi', 'male', '["cough"]', 62838, 'address10', '12:00', '2022-10-11')
-	;
+list_jenis_kelamin = ['', 'Perempuan', 'Laki-laki'] 
+
+conn = st.connection("postgresql", type="sql", 
+                     url="postgresql://famitawibi20:y2abk9QcReHn@ep-patient-field-58242561.us-east-2.aws.neon.tech/web?options=endpoint%3Dep-patient-field-58242561")
+
+st.header('DATABASE SEBARAN ALUMNI MAHASISWA STATISTIKA BISNIS')
+page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data","Statistik Alumni","Statistik Jenis Kelamin"])
+
+if page == "View Data":
+    data = conn.query('SELECT * FROM sebaran_pekerjaan ORDER By id;', ttl="0").set_index('id')
+    st.dataframe(data)
+
+if page == "Statistik Alumni":
+    st.subheader('Statistik Alumni')
+    total_alumni = len(conn.query('SELECT * FROM sebaran_pekerjaan;', ttl="0"))
+    st.write(f'Total Alumni: {total_alumni}')
+
+    st.subheader("Sebaran Angkatan Alumni")
+    data = conn.query('SELECT angkatan, COUNT(*) as count FROM sebaran_pekerjaan GROUP BY angkatan;', ttl="0")
+    st.bar_chart(data.set_index('angkatan'), color="#45FFCA")
+
+if page == "Statistik Jenis Kelamin":
+    st.subheader("Statistik Jenis Kelamin")
+    data = conn.query('SELECT jenis_kelamin, COUNT(*) as count FROM sebaran_pekerjaan GROUP BY jenis_kelamin;', ttl="0")
+    data['jenis_kelamin'] = data['jenis_kelamin'].apply(lambda x: 'Laki-laki 👨' if x == 'Laki-laki' else 'Perempuan 👩')
+    rotated_data = data.copy()
+    rotated_data['jenis_kelamin'] = rotated_data['jenis_kelamin'].apply(lambda x: x[::-1])
+    st.bar_chart(data.set_index('jenis_kelamin'), color="#FB2576")
+
+if page == "Edit Data":
+    if st.button('Tambah Data'):
+        with conn.session as session:
+            query = text('INSERT INTO sebaran_pekerjaan (nama_mahasiswa, nrp_mahasiswa, jenis_kelamin, angkatan, alamat_domisili, email, handphone, sosmed, nama_instansi, jabatan, alamat_instansi)\
+                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11);')
+            session.execute(query, {'1': '', '2': '', '3': '', '4': '', '5': '', '6': '', '7': '', '8': ' ', '9': '', '10': ' ', '11': ''})
+            session.commit()
+
+    data = conn.query('SELECT * FROM sebaran_pekerjaan ORDER By id;', ttl="0")
+    for _, result in data.iterrows():        
+        id = result['id']
+        nama_mahasiswa_lama = result["nama_mahasiswa"]
+        nrp_mahasiswa_lama = result["nrp_mahasiswa"]
+        jenis_kelamin_lama = result["jenis_kelamin"]
+        angkatan_lama = result["angkatan"]
+        alamat_domisili_lama = result["alamat_domisili"]
+        email_lama = result["email"]
+        handphone_lama = result["handphone"]
+        sosmed_lama = result["sosmed"]
+        nama_instansi_lama = result["nama_instansi"]
+        jabatan_lama = result["jabatan"]
+        alamat_instansi_lama = result["alamat_instansi"]
+
+        with st.expander(f'a.n. {nama_mahasiswa_lama}'):
+            with st.form(f'data-{id}'):
+                nama_mahasiswa_baru = st.text_input("nama_mahasiswa", nama_mahasiswa_lama)
+                nrp_mahasiswa_baru = st.text_input("nrp_mahasiswa", nrp_mahasiswa_lama)
+                jenis_kelamin_baru = st.selectbox("jenis_kelamin", list_jenis_kelamin, list_jenis_kelamin.index(jenis_kelamin_lama))
+                angkatan_baru = st.text_input("angkatan", angkatan_lama)
+                alamat_domisili_baru = st.text_input("alamat_domisili", alamat_domisili_lama)
+                email_baru = st.text_input("email", email_lama)
+                handphone_baru = st.text_input("handphone", handphone_lama)
+                sosmed_baru = st.text_input("sosmed", sosmed_lama)
+                nama_instansi_baru = st.text_input("nama_instansi", nama_instansi_lama)
+                jabatan_baru = st.text_input("jabatan", jabatan_lama)
+                alamat_instansi_baru = st.text_input("alamat_instansi", alamat_instansi_lama)
+
+                col1, col2 = st.columns([1, 6])
+
+                with col1:
+                    if st.form_submit_button('UPDATE'):
+                        with conn.session as session:
+                            query = text('UPDATE sebaran_pekerjaan\
+                                          SET nama_mahasiswa=:1, nrp_mahasiswa=:2, jenis_kelamin=:3, angkatan=:4, \
+                                          alamat_domisili=:5, email=:6, handphone=:7, sosmed=:8 , nama_instansi=:9,  jabatan=:10, alamat_instansi=:11 \
+                                          WHERE id=:12;')
+                            session.execute(query, {'1':nama_mahasiswa_baru, '2':nrp_mahasiswa_baru, '3':jenis_kelamin_baru, '4':angkatan_baru, 
+                                                    '5':alamat_domisili_baru, '6':email_baru, '7':handphone_baru, '8':sosmed_baru, '9':nama_instansi_baru, '10': jabatan_baru, '11':alamat_instansi_baru, '12':id})
+                            session.commit()
+                            st.rerun()
+                
+                with col2:
+                    if st.form_submit_button('DELETE'):
+                        with conn.session as session:
+                            query = text(f'DELETE FROM sebaran_pekerjaan WHERE id=:1')
+                            session.execute(query, {'1':id})
+                            session.commit()
+                            st.rerun()
